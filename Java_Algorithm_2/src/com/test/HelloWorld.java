14d6815a651083d53c7af7e0094297a66830547d
David J. Lim <theagape@gmail.com>
2018-03-05 22:45:04 -0800

    # WARNING: head commit changed in the meantime
    aaa

---
+++ Java_Algorithm_2/src/com/test/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author theagape
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
